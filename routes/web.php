fb2d2abfa9d6d5714f664dde831475d04f49b057
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-07 20:06:03 -0300

    home-template/hero-component

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('pages.home');
 });
 
 /* Route::middleware([
@@ -29,5 +29,5 @@ Route::get('/', function () {
 
 
 Route::get('/home', function () {
-    return view('home');
+    return view('pages.home');
 })->name('home');
