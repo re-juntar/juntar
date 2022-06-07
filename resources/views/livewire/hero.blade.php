f1a16ef51590f036f1dc4e3b7ba8af79ee5c63a7
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-07 20:34:01 -0300

    hotfix

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -3,9 +3,3 @@
     
 
 </header>
-
-<div class="bg-softViolet/80 rounded-t-xl md:rounded-r-xl md:rounded-l-none">
-    <div class="mix-blend-soft-light h-full">
-      <div class="bg-cover h-full bg-[url('../images/image-header-mobile.jpg')]"></div>
-    </div>
-</div>
