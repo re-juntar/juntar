110496f91946fb07218b2317bcae0bef14bd0575
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-08 19:11:41 -0300

    hero done

--- resources/views/components/containers/hero.blade.php
+++ resources/views/components/containers/hero.blade.php
@@ -1,6 +1,6 @@
 <header
     class="min-h-[50vh] bg-center bg-[url('https://images.unsplash.com/photo-1456428746267-a1756408f782?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')] bg-no-repeat bg-cover relative flex before:absolute before:left-0 before:top-0 before:h-full before:w-full before:block before:opacity-80 before:transition-opacity before:duration-300 before:ease-linear before:bg-gradient-radial">
     <div class="flex flex-col items-center m-auto z-[2] py-[7vh] w-[700px]">
-        
+        {{ $slot }}
     </div>
 </header>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,9 @@
 <x-app-layout>
-    <livewire:hero />
+    <x-hero>
+        <img class="max-w-full h-auto pl-[10vw] pr-[10vh] mb-4" src="{{asset('images/logos/juntar-logo-w.svg')}}" alt="" />
+        <h5 class="text-white uppercase font-medium text-xl mb-4">Sistema de gestión de eventos</h5>
+        <x-jet-button class="bg-[#FE1355] uppercase text-lg hover:bg-[#050714]">Empezar</x-jet-button>
+    </x-hero>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
