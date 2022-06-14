8a75cd3d916584760015f075a8a22e02fa862297
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-14 18:57:52 -0300

    .

--- app/Http/Livewire/AboutUsV1.php
+++ app/Http/Livewire/AboutUsV1.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class AboutUsV1 extends Component
 {
+
+    public $params = [
+        '0' => [
+            'nombre' => 'name',
+            'descripcion' => 'description',
+            'link' => 'link',
+            'isDev' => true
+        ]
+    ];
+
     public function render()
     {
         return view('livewire.about-us-v1');
--- resources/views/livewire/about-us-v1.blade.php
+++ resources/views/livewire/about-us-v1.blade.php
@@ -2,20 +2,14 @@
     <h2 class="mb-[1rem]">Equipo Programación Web Avanzada - 2020 (Juntar v1) </h2>
     <hr class="my-[1rem]">
     <div class="flex flex-wrap w-full gap-4">
+        @foreach ($params as $dev)
+            
         <x-about-us-card>
-            <x-slot name='nombre' class="text-6xl">Federico Diaz A.</x-slot>
-            <x-slot name='descripcion'>Meti mano en el Front</x-slot>
-            <x-slot name='link'>https://www.linkedin.com/in/diazaimar/</x-slot>
+            <x-slot name='nombre' class="text-6xl">{{$dev['nombre']}}</x-slot>
+            <x-slot name='descripcion'>{{$dev['descripcion']}}</x-slot>
+            <x-slot name='link'>{{$dev['link']}}</x-slot>
         </x-about-us-card>
-        <x-about-us-card>
-            <x-slot name='nombre' class="animation-gradient-anima">Federico Diaz A.</x-slot>
-            <x-slot name='descripcion'>Meti mano en el Front</x-slot>
-            <x-slot name='link'>https://www.linkedin.com/in/diazaimar/</x-slot>
-        </x-about-us-card>
-        <x-about-us-card>
-            <x-slot name='nombre' class="animation-gradient-anima">Federico Diaz A.</x-slot>
-            <x-slot name='descripcion'>Meti mano en el Front</x-slot>
-            <x-slot name='link'></x-slot>
-        </x-about-us-card>
+        @endforeach
+        
     </div>
 </div>
