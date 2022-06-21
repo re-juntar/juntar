d90612f741fcdaa6143fe30f3788df26854f6556
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-21 19:22:55 -0300

    Verified badge for events

---
+++ resources/views/components/verified-badge.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes->merge(['class' => 'inline-flex items-center bg-fogra-darkish rounded-xl shadow-lg p-1'])}}>
+    <img class="h-10 w-10" src="{{asset('images/logos/logo-fai-w.png')}}" alt="">
+    <div class="text-white-ghost text-sm">
+        <h1>Verificado</h1>
+        <h2>FAI</h2>
+    </div>
+</div>
