60457a635b93542fe72e86d17a8a38d0e7b8d9ff
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-22 18:54:50 -0300

    unused components

--- resources/views/components/backend/dropdown-link.blade.php
+++
@@ -1,1 +0,0 @@
-<a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="primary">Link 3</a>
