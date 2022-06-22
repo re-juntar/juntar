d5a708f021a556b32c18c6f991e95820bad46c2c
Juan Marcos Gonzalez <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-06-21 21:11:37 -0300

    💬 Updated endorsement text

--- resources/views/components/verified-badge.blade.php
+++ resources/views/components/verified-badge.blade.php
@@ -1,7 +1,7 @@
-<div {{ $attributes->merge(['class' => 'inline-flex items-center bg-fogra-darkish rounded-xl shadow-lg p-1'])}}>
-    <img class="h-10 w-10" src="{{asset('images/logos/logo-fai-w.png')}}" alt="">
-    <div class="text-white-ghost text-sm">
-        <h1>Verificado</h1>
-        <h2>FAI</h2>
+<div {{ $attributes->merge(['class' => 'inline-flex items-center bg-fogra-darkish rounded-xl shadow-lg p-1']) }}>
+    <img class="h-6 w-6 ml-1 mr-2" src="{{ asset('images/logos/logoFAI.png') }}" alt="">
+    <div class="text-white-ghost text-sm mr-2">
+        <h2>Avalado</h2>
+        <h3>FAI</h3>
     </div>
 </div>
