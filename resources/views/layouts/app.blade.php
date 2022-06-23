9ca72b8bbe8accbc651903ccae4430c0174c6f5d
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-23 16:42:38 -0300

    fonts config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
 
     <title>{{ config('app.name', 'Juntar') }}</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <!-- FontAwesome -->
