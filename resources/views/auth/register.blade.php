4f4c558739d73fb6e6205c0251ac18372fb7dad3
Juan Marcos Gonzalez <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-06-23 21:02:51 -0300

    🩹💬 Fixed and added texts in register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,72 @@
 <x-app-layout>
-    <x-jet-authentication-card>
+    <x-jet-authentication-card class="my-6">
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-jet-authentication-card-logo class="mt-4" />
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
-
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div class="mt-4">
-                <x-jet-label for="name" value="{{ __('Nombre') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                    autofocus autocomplete="name" />
+                <h2 class="text-2xl font-bold">Crear Cuenta</h2>
+                <h3 class="text-lg">Complete el formulario para registrarse en el sistema</h3>
+                <span class="text-gray-600 text-sm">Los campos marcados con (*) son obligatorios</span>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="surname" value="{{ __('Apellido') }}" />
-                <x-jet-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required
-                    autofocus autocomplete="surname" />
+                <x-jet-label for="name" value="{{ __('Nombre (*)') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+                    required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="dni" value="{{ __('DNI') }}" />
-                <x-jet-input id="dni" class="block mt-1 w-full" type="text" name="dni" :value="old('dni')" required
-                    autofocus autocomplete="dni" />
+                <x-jet-label for="surname" value="{{ __('Apellido (*)') }}" />
+                <x-jet-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')"
+                    required autofocus autocomplete="surname" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="country" value="{{ __('Country') }}" />
-                <x-jet-input id="country" class="block mt-1 w-full" type="text" name="country" :value="old('country')" required
-                    autofocus autocomplete="country" />
-            </div>
-
-            
-
-            <div class="mt-4">
-                <x-jet-label for="province" value="{{ __('Provincia') }}" />
-                <x-jet-input id="province" class="block mt-1 w-full" type="text" name="province" :value="old('province')" required
-                    autofocus autocomplete="province" />
+                <x-jet-label for="dni" value="{{ __('DNI (*)') }}" />
+                <x-jet-input id="dni" class="block mt-1 w-full" type="text" name="dni" :value="old('dni')"
+                    required autofocus autocomplete="dni" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="city" value="{{ __('Ciudad') }}" />
-                <x-jet-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required
-                    autofocus autocomplete="city" />
+                <x-jet-label for="country" value="{{ __('País (*)') }}" />
+                <x-jet-input id="country" class="block mt-1 w-full" type="text" name="country" :value="old('country')"
+                    required autofocus autocomplete="country" />
+            </div>
+
+
+
+            <div class="mt-4">
+                <x-jet-label for="province" value="{{ __('Provincia (*)') }}" />
+                <x-jet-input id="province" class="block mt-1 w-full" type="text" name="province" :value="old('province')"
+                    required autofocus autocomplete="province" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Correo Electrónico') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-label for="city" value="{{ __('Ciudad (*)') }}" />
+                <x-jet-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')"
+                    required autofocus autocomplete="city" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
+                <x-jet-label for="email" value="{{ __('Correo Electrónico (*)') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Contraseña (*)') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirme su Contraseña') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirme su Contraseña (*)') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password"
                     name="password_confirmation" required autocomplete="new-password" />
             </div>
@@ -84,7 +90,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('¿Ya se ha registrado?') }}
+                    {{ __('¿Ya se encuentra registrado?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
