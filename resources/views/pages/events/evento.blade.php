ceeac268253362497e554658f97c0a5facb0099d
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-06-23 21:52:47 -0300

    Responsive para la vista de eventos.

--- resources/views/pages/events/evento.blade.php
+++ resources/views/pages/events/evento.blade.php
@@ -1,7 +1,7 @@
 @php
   $flyerSrc = $event['image_flyer'];
   if ($event['image_flyer'] == null) {
-      $flyerSrc = 'Flyer en construcción';
+      $flyerSrcNull = 'Flyer en construcción';
   }
   $logoSrc = $event['image_logo'];
   if ($event['image_logo'] == null) {
@@ -11,14 +11,14 @@
 
 <x-app-layout>
   <x-hero>
-    <h5 class="text-white-ghost uppercase font-medium text-[2.5rem] mb-4 text-center leading-[1.2]">{{$event->name}}
+    <h5 class="text-white-ghost uppercase font-medium text-[1.5rem] md:text-[2.5rem] mb-4 text-center leading-[1.2]">{{$event->name}}
     </h5>
-    <h5 class="text-white-ghost uppercase font-medium text-[24px] mb-[0.5rem]"><i class="fa-regular fa-calendar mr-3"></i>{{$event->start_date}}</h5>
-    <h5 class="text-white-ghost uppercase font-medium text-[24px] mb-[0.5rem]"><i class="fa-solid fa-location-dot mr-3"></i>{{$event->modality}}</h5>
+    <h5 class="text-white-ghost uppercase font-medium text-[16px] md:text-[24px] mb-[0.5rem]"><i class="fa-regular fa-calendar mr-3"></i>{{$event->start_date}}</h5>
+    <h5 class="text-white-ghost uppercase font-medium text-[16px] md:text-[24px] mb-[0.5rem]"><i class="fa-solid fa-location-dot mr-3"></i>{{$event->modality}}</h5>
     <h5 class="text-white-ghost uppercase font-medium text-[20px] mb-[0.5rem]">
     @if($event->endorsed)    
       <h5 class="text-white-ghost uppercase font-medium text-[20px] mb-[0.5rem]">
-        Evento avalado por la FAI 
+        Evento avalado por la FAI {{-- Componente --}}
       </h5>
     @endif
         
@@ -27,7 +27,7 @@
     <div class="event-container max-w-md md:max-w-3xl lg:max-w-4xl xl:max-w-7xl mx-auto rounded-[0.25rem] py-[7vh] px-[3vh]">
       <div class="event-body bg-[#fff]">
         {{-- Header --}}
-        <x-pink-header class="h-[50px] text-[#856404]  bg-[#fff3cd] uppercase">
+        <x-pink-header class="text-[#856404]  bg-[#fff3cd] uppercase h-auto">
           @if($event->end_date<=date("Y-m-d"))
             El evento se encuentra finalizado
           @elseif($event->start_date<=date("Y-m-d")&&$event->end_date>=date("Y-m-d"))
@@ -38,8 +38,8 @@
         </x-pink-header>
         <x-pink-header class="h-[50px] border-0 rounded-none"></x-pink-header>
         {{-- Hero --}}
-        <div class="event-body-hero flex p-[15px] ">
-          <div class="general-info flex flex-col justify-around items-start w-full md:w-8/12">
+        <div class="event-body-hero flex flex-col md:flex md:flex-row p-[15px] ">
+          <div class="general-info flex flex-col justify-around items-start w-full md:w-8/12 md:flex">
             <p class="text-[16px] mb-[0.5rem] flex items-center justify-center"><i class="fa-regular fa-calendar mr-3"></i>{{$event->start_date}}</p>
             <h2 class="text-[2rem] mb-[0.5rem] font-bold">{{$event->name}}</h2>
             @if($event->user_id == null)
@@ -48,7 +48,7 @@
               <p>Evento organizado por User con ID {{$event->user_id}}</p>
             @endif
             @if($event['image_flyer'])
-              <a href="{{asset($flyerSrc)}}" download>
+              <a class="text-[16px]" href="{{asset($flyerSrc)}}" download>
                 <x-button class="text-[16px]">
                   <i class="fas fa-file-download mr-3"></i>Flyer
                 </x-button>
@@ -59,17 +59,17 @@
                 </x-button>
             @endif
           </div>
-          <div class="flex flex-col w-full md:w-4/12">
-            {{-- <div class="event-flyer">
+          <div class="event-flyer-and-logo py-[1rem] flex flex-col items-center justify-center md:flex w-full md:w-4/12">
+            <div class="event-flyer">
               @if($event['image_flyer'])
-                <img class="rounded-lg" src="{{asset($flyerSrc)}}" alt="Flyer {{$event->name}}">
+                <img class="rounded-lg w-full max-w-[80%] mx-auto" src="{{asset($flyerSrc)}}" alt="Flyer {{$event->name}}">
               @else
                 {{$flyerSrc}}
               @endif
-            </div> --}}
-            <div class="event-logo w-1/2">
+            </div> 
+            <div class="event-logo pt-[0.5rem]">
               @if($event['image_logo'])
-                <img class="rounded-lg" src="{{ asset($logoSrc) }}" alt="Logo {{$event->name}}">
+                <img class="rounded-lg max-w-[50%] mx-auto" src="{{ asset($logoSrc) }}" alt="Logo {{$event->name}}">
               @else
                 {{$logoSrcNull}}
               @endif
@@ -77,7 +77,7 @@
           </div>
         </div>
         {{-- Preinscripcion/Inscripcion --}}
-        <div class="event-body-inscription flex py-[3vh] bg-[#F2F2F2]">
+        <div class="event-body-inscription flex flex-col md:flex md:flex-row py-[3vh] bg-[#F2F2F2]">
           <div class="quota flex items-center w-full md:w-8/12 px-[15px]">
             @if($event->capacity > 0)
               <p>CUPOS DISPONIBLES: {{$event->capacity}} 
@@ -90,7 +90,7 @@
           </div>
           <div class="status w-full md:w-4/12 px-[15px]">
             @if($event->start_date>=date("Y-m-d"))
-              <x-button class="bg-cyan-500 mr-2">Inscribirse</x-button>
+              <x-button class="bg-cyan-500 mr-2 text-[16px]">Inscribirse</x-button>
             @elseif($event->start_date<=date("Y-m-d") && $event->end_date>date("Y-m-d"))
               <p>El evento ya ha iniciado</p>
             @elseif($event->end_date<=date("Y-m-d"))
@@ -99,16 +99,21 @@
           </div>
         </div>
         {{-- Main Content --}}
-        <div class="event-body-main flex py-[3rem]">
-          <div class="event-info flex flex-col justify-start overflow-auto items-start w-full md:w-8/12 px-[15px]">
+        <div class="event-body-main flex-col flex-wrap md:flex md:flex-row pb-[3rem] p-[15px]">
+          {{-- Descripcion --}}
+          <div class="event-info flex flex-col justify-start items-start w-full md:w-8/12 xl:w-9/12 pr-3 pb-3">
+            <h2 class="text-[1.4rem] font-bold">Sobre este evento</h2>
             @if($event->description)
-              {!!$event->description!!}
+              <div class="w-full">
+                {!!$event->description!!}
+              </div>
             @else
               <p>No hay descripcion del evento.</p>
             @endif
           </div>
-          <div class="more-event-info w-full md:w-4/12 px-[15px] mx-[15px] text-white-ghost bg-[#0B0D19]">
-            <ul class=" flex flex-col justify-center items-start">
+          {{-- Mas info ("Tabla") --}}
+          <div class="more-event-info w-full md:w-4/12 xl:w-3/12  text-white-ghost px-[15px] bg-[#0B0D19]">
+            <ul class=" flex flex-col justify-center items-start w-10/12 mx-auto">
               <li class="py-[0.75rem] ">
                 <p class="mb-[1rem] font-bold">Fecha de Inicio:</p>
                 <span>{{$event->start_date}}</span>
