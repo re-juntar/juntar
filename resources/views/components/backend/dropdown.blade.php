45f3403a3852b79fd0c713eaeb1bef1b6d033720
Raam4 <ramiro_cardozo22@hotmail.com>
2022-06-24 15:35:07 -0300

    Changes to dropdown component

--- resources/views/components/backend/dropdown.blade.php
+++ resources/views/components/backend/dropdown.blade.php
@@ -1,5 +1,5 @@
-<button x-data="{ open: false }" x-on:click="open = true" @click.outside="open = false" {{ $attributes->merge(['class' => 'group focus:outline-none w-full']) }}  :class="open ? 'bg-fogra-darkish' : ''">
-    <div class="flex items-center justify-center text-lg py-4 px-6 h-12 text-gray-500 decoration-2 font-semibold leading-5 transition">
+<div x-data="{ open: false }" x-on:click="open = ! open " @click.outside="open = false" {{ $attributes->merge(['class' => 'w-full']) }}  :class="open ? 'bg-fogra-darkish' : ''">
+    <div class="flex items-center justify-center text-lg py-4 px-6 h-12 text-gray-500 decoration-2 font-semibold leading-5 cursor-pointer transition duration-400">
     @if( isset($profile))
         {{ $profile }}
     @else
@@ -8,7 +8,7 @@
         <i x-show="!open" class="fa fa-caret-down ml-1"></i>
         <i x-show="open" class="fa fa-caret-up ml-1 text-gray-100"></i>
     </div>
-    <div class="max-h-0 overflow-hidden duration-300 group-focus:max-h-40">
+    <div x-show="open" class="h-fit" x-transition.duration.400ms>
         {{ $links }}
     </div>
-</button>
+</div>
