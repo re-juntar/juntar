469b9560eb1dfaf00b7bb4cdeea4c184fa612682
cristian96-code <cristian.garrado@est.fi.uncoma.edu.ar>
2022-06-28 22:02:34 -0300

    vista Avales.

--- app/Http/Livewire/Backend/EndorsementTable.php
+++ app/Http/Livewire/Backend/EndorsementTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Livewire\Backend;
 
-use Rappasoft\LaravelLivewireTables\DataTableComponent;
-use Rappasoft\LaravelLivewireTables\Views\Column;
+use Carbon\Carbon;
 use App\Models\EndorsementRequest;
+use Illuminate\Database\DBAL\TimestampType;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
 
 class EndorsementTable extends DataTableComponent
 {
@@ -17,12 +19,52 @@ class EndorsementTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+
+        $this->setHideBulkActionsWhenEmptyEnabled();
+
+        $this->setEmptyMessage('No se encontraron Avales');
+
+        $this->setQueryStringDisabled();
+
+        $this->setTableAttributes(['class' => ""] );
+
+        
     }
 
     public function columns(): array
     {
         return [
-            Column::make("ID Usuario", "id"),
+            Column::make("ID Avals", "id"),
+            Column::make("Nombre Evento", 'event.name'),
+            Column::make("Solicitante", "user.name"),
+            Column::make("Token", 'request_token'),
+            Column::make("Fecha de revision", "revision_date"),
+            Column::make("Estado", "endorsed"),
+            
         ];
     }
+
+    public function bulkActions(): array
+    {
+        return [
+            'acceptEndorsement' => 'Aceptar Aval',
+            'denyEndorsement' => 'Rechazar Aval',
+        ];
+    }
+
+    public function acceptEndorsement()
+    {
+        EndorsementRequest::whereIn('id', $this->getSelected())->update(['endorsed' => 1]);
+        EndorsementRequest::whereIn('id', $this->getSelected())->update(['revision_date' => Carbon::now()]);
+
+        
+        $this->clearSelected();
+    }
+    public function denyEndorsement()
+    {
+        EndorsementRequest::whereIn('id', $this->getSelected())->update(['endorsed' => 0]);
+
+        $this->clearSelected();
+    }
 }
+
