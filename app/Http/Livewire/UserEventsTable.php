e2555358fefccb0bfdc83a772da0ad760193e8d9
santiagoavilez <santiagolingreed@gmail.com>
2022-07-20 17:11:51 -0300

    Terminado con las tablas de mis eventos: responsive y botones.

--- app/Http/Livewire/UserEventsTable.php
+++ app/Http/Livewire/UserEventsTable.php
@@ -8,7 +8,9 @@ use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Event;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
 use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+
 
 class UserEventsTable extends DataTableComponent
 {
@@ -37,7 +39,7 @@ class UserEventsTable extends DataTableComponent
 
          $this->setComponentWrapperAttributes([
              'id' => 'eventos',
-             'class' => ' text-black bg-gray-200 p-3',
+             'class' => ' text-black bg-gray-200 pt-3 pb-1 lg:p-3 px-3 ',
            ]);
 
 
@@ -64,17 +66,47 @@ class UserEventsTable extends DataTableComponent
     public function columns(): array
     {
         return [
-            LinkColumn::make('')
-                ->title(fn ($row) => 'VER EVENTO')
-                ->location(fn ($row) => route('evento', ['eventoId' => $row['id']])),
-        LinkColumn::make('')
-                ->title(fn ($row) => 'EDITAR EVENTO')
-                ->location(fn ($row) => route('edit-event', ['eventId' => $row['id']])),
-            Column::make("Id", "id")
-                ->sortable(),
             Column::make("Nombre", "name"),
+        //     LinkColumn::make('Accion')
+        //         ->title(fn ($row) => 'VER EVENTO')
+        //         ->location(fn ($row) => route('evento', ['eventoId' => $row['id']]))->collapseOnMobile(),
+        // LinkColumn::make('Accion')                
+        //         ->title(fn ($row) => 'EDITAR EVENTO')
+        //         ->location(fn ($row) => route('edit-event', ['eventId' => $row['id']]))
+        //         ->attributes(fn($row) => [
+        //             'class' => 'rounded-full text-white',
+        //             'alt' => $row->name . ' Avatar',
+        //         ]),             
+            Column::make("Id", "id")
+                ->sortable()->collapseOnMobile(),
+            
             // Column::make("Nombre Corto", "short_name"),
-            Column::make("Estado", 'eventStatus.description')
+            Column::make("Estado", 'eventStatus.description')->collapseOnMobile(),
+            ButtonGroupColumn::make('Acciones')
+            ->attributes(function($row) {
+                return [
+                    'class' => 'space-x-2',
+                ];
+            })
+            ->buttons([
+                LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
+                    ->title(fn($row) => ' ')
+                    ->location(fn ($row) => route('evento', ['eventoId' => $row['id']]))
+                    ->attributes(function($row) {
+                        return [
+                            'class' =>'fa-solid fa-eye border border-1 border-black rounded p-2 text-blue-100 hover:no-underline',
+                        ];
+                    }),
+                LinkColumn::make('Edit')
+                    ->title(fn($row) => ' ' )
+                    ->location(fn ($row) => route('edit-event', ['eventId' => $row['id']]))
+                    ->attributes(function($row) {
+                        return [
+                            'target' => '_blank',
+                            'class' => 'text-red-500 border border-1 border-black rounded bg-blue-500  fa-solid fa-pen-to-square p-2 hover:no-underline',
+                        ];
+                    }),
+            ])->collapseOnMobile(),
             // Column::make("Nombre Organizador", "user.name"),
             // Column::make("Apellido Organizador", 'user.surname')
             // Column::make("Created at", "created_at")
