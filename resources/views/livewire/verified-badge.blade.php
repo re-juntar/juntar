dd7dc9cc22e8857b60c1648f20974b9251c102cb
cristian96-code <cristian.garrado@est.fi.uncoma.edu.ar>
2022-07-23 00:37:35 -0300

    visualizacion de logo de aval en front acorregido.

--- resources/views/livewire/verified-badge.blade.php
+++ resources/views/livewire/verified-badge.blade.php
@@ -1,10 +1,10 @@
 <div class= 'inline-flex items-center bg-fogra-darkish rounded-xl shadow-lg p-1'>
     @foreach ($academicUnits as $academisUnit)
         @if ($academisUnit->id === $endorsementRequest->academic_units_id)
-            @if ($academisUnit->image_logo === null || !file_exists(asset('images/logos/'.$academisUnit->image_logo)))
-                <img class="h-6 w-6 ml-1 mr-2" src="{{ asset('images/logos/logo-uncoma-w.svg') }}" alt="">
+            @if ($academisUnit->image_logo != null && file_exists('images/logos/'.$academisUnit->image_logo))
+                <img class="h-6 w-6 ml-1 mr-2" src="{{ asset('images/logos/'.$academisUnit->image_logo) }}" alt="">            
             @else
-                <img class="h-6 w-6 ml-1 mr-2" src="{{ asset('images/logos/'.$academisUnit->image_logo) }}" alt="">
+                <img class="h-6 w-6 ml-1 mr-2" src="{{ asset('images/logos/logo-uncoma-w.svg') }}" alt="">
             @endif
             <div class="text-white-ghost text-sm mr-2">
                 <h2>Avalado</h2>
