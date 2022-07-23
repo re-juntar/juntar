be39a1b5fb92219e6bf6a1c5b99358a9e03746da
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2022-07-23 17:15:17 -0300

    fix estilo de la tabla

--- app/Http/Livewire/InscriptionTable.php
+++ app/Http/Livewire/InscriptionTable.php
@@ -7,6 +7,8 @@ use App\Models\Inscription;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
 
 class InscriptionTable extends DataTableComponent
 {
@@ -20,21 +22,50 @@ class InscriptionTable extends DataTableComponent
     {
         return Inscription::where('event_id', $this->event)->where('inscription_date', '<>', 'null');
     }
-
+    
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+     
+        $this->setHideBulkActionsWhenEmptyEnabled();
         $this->setQueryStringDisabled();
+        $this->setColumnSelectDisabled();
+
+        $this->setComponentWrapperAttributes([
+            'id' => 'Id',
+            'class' => ' text-black bg-gray-200 pt-3 pb-1 lg:p-3 px-3 ',
+          ]);
+
+          $this->setTrAttributes(function($row, $index) {
+            if ($index % 2 === 0) {
+              return [
+                'default' => false,
+                'class' => 'bg-gray-300 text-black',
+              ];
+            }
+            else{
+               return [
+                 'default' => false,
+                 'class' => 'bg-white-ghost text-black',
+               ];
+             }
+
+            return ['default' => false];
+        });
     }
 
     public function columns(): array
     {
         return [
+            
+            
             Column::make("ID", "id")->hideIf(true),
+            Column::make("Evento", "event.name"),
             Column::make("Nombre", "user.name"),
             Column::make("Apellido", "user.surname"),
             Column::make("DNI", "user.dni"),
-            Column::make("Fecha", "inscription_date")
+            Column::make("Fecha de inscripcion", "inscription_date")
+        
         ];
     }
 
