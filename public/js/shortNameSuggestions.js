17726ae876bc5f81517a2c874484ee1659fde1a5
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-07-27 20:44:57 -0300

    Modularizacion. Agregado de un label y estilos.

--- public/js/shortNameSuggestions.js
+++ public/js/shortNameSuggestions.js
@@ -1,118 +1,85 @@
 /* Selectors */
 let nameInput = document.getElementById("name");
 let shortNameInput = document.getElementById("short-name");
-let automaticSlug = document.getElementById("automaticSlug");
+let automaticOptions = document.getElementById("automaticSlug");
 
 /* Listeners */
-nameInput.addEventListener("keyup", () => {
-    automaticSlug.replaceChildren();
+nameInput.addEventListener("blur", () => {
+    automaticOptions.replaceChildren();
     if (nameInput.value.trim() != "") {
-        console.log(nameInput.value);
+        let automaticOptionsLabel = document.createElement("label");
+        automaticOptionsLabel.textContent = "Opciones Automáticas: ";
+        automaticOptions.appendChild(automaticOptionsLabel);
         generateShortNameOptions(eliminateDiacritic(nameInput.value));
+        automaticOptions.setAttribute("class", "mb-2");
     }
 });
 
 /* Functions */
 function generateShortNameOptions(eventName) {
-    automaticSlug.appendChild(generateLowercaseShortName(eventName));
-    automaticSlug.appendChild(generateInitialsWithYearShortName(eventName));
-    automaticSlug.appendChild(generateYearShortName(eventName));
-    automaticSlug.appendChild(generateOtherOptionShortName());
+    automaticOptions.appendChild(generateInput(eventName, "opt1"));
+    automaticOptions.appendChild(generateInput(eventName, "opt2"));
+    automaticOptions.appendChild(generateInput(eventName, "op3"));
+    automaticOptions.appendChild(generateInput(eventName, "other"));
 }
 
-function generateLowercaseShortName(eventName) {
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-
-    let label = document.createElement("label");
-    let slug = eventName
-        .toLowerCase()
-        .replace(/[^\w ]+/g, "")
-        .replace(/ +/g, "-");
-    input.setAttribute("type", "radio");
-    input.setAttribute("id", "opc1");
-    input.setAttribute("name", "shortName");
-    input.setAttribute("value", slug);
-    label.setAttribute("for", "opc1");
-    label.setAttribute("class", "ml-2");
-    label.textContent = slug;
-    div.appendChild(input);
-    div.appendChild(label);
-    input.addEventListener("click", () => {
-        shortNameInput.value = input.value;
-    });
-    return div;
-}
-
-function generateInitialsWithYearShortName(eventName) {
+function generateInput(eventName, option) {
     let div = document.createElement("div");
     let input = document.createElement("input");
     let label = document.createElement("label");
     let year = new Date().getFullYear();
-    let inicialesYear = eventName.match(/\b(\w)/g).join("");
-    inicialesYear += year;
+    let shortName = "";
+    switch (option) {
+        case "opt1":
+            shortName = eventName
+                .toLowerCase()
+                .replace(/[^\w ]+/g, "")
+                .replace(/ +/g, "-");
+            input.setAttribute("value", shortName);
+            break;
+        case "opt2":
+            let eventNameInitials = eventName.match(/\b(\w)/g).join("");
+            shortName += eventNameInitials + year;
+            input.setAttribute("value", shortName);
+            break;
+        case "op3":
+            shortName +=
+                year +
+                "-" +
+                eventName
+                    .toLowerCase()
+                    .replace(/[^\w ]+/g, "")
+                    .replace(/ +/g, "-")
+                    .split("-")
+                    .slice(0, 2)
+                    .join("-");
+            input.setAttribute("value", shortName);
+            break;
+        case "other":
+            shortName += "Otro: ";
+            input.setAttribute("value", "");
+            break;
+        default:
+            shortName += "Otro: ";
+            input.setAttribute("value", "");
+            break;
+    }
     input.setAttribute("type", "radio");
-    input.setAttribute("id", "opc2");
+    input.setAttribute("id", `${option}`);
     input.setAttribute("name", "shortName");
-    input.setAttribute("value", inicialesYear);
-    label.setAttribute("for", "opc2");
+    label.setAttribute("for", `${option}`);
     label.setAttribute("class", "ml-2");
-    label.textContent = inicialesYear;
+    label.textContent = shortName;
     div.appendChild(input);
     div.appendChild(label);
+    div.setAttribute("class", "ml-4");
     input.addEventListener("click", () => {
         shortNameInput.value = input.value;
-    });
-    return div;
-}
-
-function generateYearShortName(nombreEvento) {
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-    let label = document.createElement("label");
-    let year = new Date().getFullYear();
-    let cortoYear = year;
-    cortoYear +=
-        "-" +
-        nombreEvento
-            .toLowerCase()
-            .replace(/[^\w ]+/g, "")
-            .replace(/ +/g, "-")
-            .split("-")
-            .slice(0, 2)
-            .join("-");
-    //
-    input.setAttribute("type", "radio");
-    input.setAttribute("id", "opc3");
-    input.setAttribute("name", "shortName");
-    input.setAttribute("value", cortoYear);
-    label.setAttribute("for", "opc3");
-    label.setAttribute("class", "ml-2");
-    label.textContent = cortoYear;
-    div.appendChild(input);
-    div.appendChild(label);
-    input.addEventListener("click", () => {
-        shortNameInput.value = cortoYear;
-    });
-    return div;
-}
-
-function generateOtherOptionShortName() {
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-    let label = document.createElement("label");
-    let year = new Date().getFullYear();
-    input.setAttribute("type", "radio");
-    input.setAttribute("id", "otro");
-    input.setAttribute("name", "shortName");
-    input.setAttribute("value", "");
-    label.setAttribute("for", "otro");
-    label.setAttribute("class", "ml-2");
-    label.textContent = "Otro: ";
-    div.appendChild(input);
-    div.appendChild(label);
-    input.addEventListener("click", () => {
-        shortNameInput.value = input.value;
+        if (input.getAttribute("id") !== "other") {
+            shortNameInput.readOnly = true;
+        } else {
+            shortNameInput.readOnly = false;
+        }
     });
     return div;
 }
@@ -120,13 +87,3 @@ function generateOtherOptionShortName() {
 function eliminateDiacritic(text) {
     return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
-/* $(document).on("click", ".nombresCortos input:radio", function () {
-    if ($(this).attr("id") !== "otro") {
-        $("#short-name").prop("readonly", true);
-        $("#short-name").prop("disabled", true);
-        $("#short-name").val($(this).val());
-    } else {
-        $("#short-name").prop("readonly", false);
-        $("#short-name").prop("disabled", false);
-    }
-}); */
