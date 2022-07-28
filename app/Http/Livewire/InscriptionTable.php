268c31b7388460921be901c2e5f3a4fe9bf98d77
cristian96-code <cristian.garrado@est.fi.uncoma.edu.ar>
2022-07-28 19:42:16 -0300

    view of my inscriptions..

--- app/Http/Livewire/InscriptionTable.php
+++ app/Http/Livewire/InscriptionTable.php
@@ -3,39 +3,99 @@
 namespace App\Http\Livewire;
 
 use App\Models\Event;
+use App\Models\EventModality;
 use App\Models\Inscription;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
 
 class InscriptionTable extends DataTableComponent
 {
-    protected $model = Inscription::class;
-
-    public $events;
+    protected $model = Inscriptions::class;
 
     protected $listeners = ['refreshComponent' => '$refresh'];
+
+    public array $inscriptions = [];
+
+    public $event;
 
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+        
         $this->setQueryStringDisabled();
+
+        $this->setHideBulkActionsWhenEmptyEnabled();
+
+        $this->setComponentWrapperAttributes([
+            'id' => 'inscriptions',
+            'class' => ' text-black bg-gray-200 pt-3 pb-1 lg:p-3 px-3 ',
+          ]);
+
+          $this->setTrAttributes(function($row, $index) {
+            if ($index % 2 === 0) {
+              return [
+                'default' => false,
+                'class' => 'bg-gray-300 text-black',
+              ];
+            }
+            else{
+               return [
+                 'default' => false,
+                 'class' => 'bg-white-ghost text-black',
+               ];
+             }
+
+            return ['default' => false];
+        });
     }
 
     public function columns(): array
     {
         return [
-            Column::make("ID", "id")->hideIf(true),
-            Column::make("Nombre", "user.name"),
-            Column::make("Apellido", "user.surname"),
-            Column::make("DNI", "user.dni"),
-            Column::make("Fecha", "inscription_date")
+            Column::make("#", "id")->hideIf(true),
+            Column::make("#", "event.id")->hideIf(true),
+            Column::make("Nombre de Evento", "event.name"),
+            Column::make("fecha de Inicio", "event.start_date"),
+            Column::make("Fecha de Inscripcion", "inscription_date"),
+            Column::make("Modalidad", "event.eventModality.description"),
+            Column::make("Link de evento", "event.meeting_link"),
+            Column::make("Ubicacion", "event.venue"),
+            ButtonGroupColumn::make('acciones')
+            ->attributes(function($row) {
+                return [
+                    'class' => 'space-x-2 ',
+                ];
+            })
+            ->buttons([
+                LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
+                    ->title(fn($row) => 'ver Evento')
+                    ->location(fn ($row) => route('evento', ['id' => $row['event.id']]))
+                    ->attributes(function($row) {
+                        return [
+                            'class' =>' border border-1 border-black rounded p-2 text-blue-100 hover:no-underline',
+                        ];
+                    }),
+            
+                LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
+                ->title(fn($row) => 'ver PyR')
+                ->location(fn ($row) => route('evento', ['id' => $row['event.id']]))
+                ->attributes(function($row) {
+                    return [
+                        'class' =>'text-green-500 border border-1 border-black rounded p-2 text-blue-100 hover:no-underline',
+                        'wire:click' => '$emit("showPyRModal")',
+                    ];
+                }),
+            ])->collapseOnMobile()
         ];
     }
 
     public function builder(): Builder
     {
-        return Inscription::whereIn('event_id', $this->events[0])->where('inscription_date', '<>', 'null');
+        return Inscription::where(['inscriptions.user_id' => Auth::user()->id ])->where('inscription_date', '<>', 'null');
     }
 
     public function bulkActions(): array
--- resources/views/livewire/my-inscriptions.blade.php
+++ resources/views/livewire/my-inscriptions.blade.php
@@ -9,10 +9,9 @@
         <div class="flex flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 class="text-white-ghost uppercase mb-2 font-medium leading-6 text-3xl">Mis inscripciones a eventos</h2>
             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-                    @isset($events[0])
-                        @livewire('inscription-table', ['events' => $events[0]])
-                    @endisset
+                <div class="overflow-hidden  rounded-lg m-1">
+                    @livewire('inscription-table')
+                    @livewire('q-and-a-modal')
                 </div>
             </div>
         </div>
