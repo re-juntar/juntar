4938b081f22a128d2a81ff68fb525a1e4a77a077
santiagoavilez <santiagolingreed@gmail.com>
2022-07-29 21:02:22 -0300

    titulos en las tablas de eventos y usuarios

--- resources/views/pages/backend/users.blade.php
+++ resources/views/pages/backend/users.blade.php
@@ -1,5 +1,8 @@
 <div class="">
     <x-card class="max-w-4xl mx-auto">
+        <section class="my-5 text-4xl text-awesome">
+            Usuarios
+        </section>
         <livewire:backend.user-table />
     </x-card>
 </div>
