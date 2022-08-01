dac8a60b6872ca465be3eb84f8980da0d0300a11
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2022-07-31 22:59:16 -0300

    ...

--- app/Http/Livewire/EventInscriptionsTable.php
+++ app/Http/Livewire/EventInscriptionsTable.php
@@ -56,39 +56,11 @@ class EventInscriptionsTable extends DataTableComponent
     {
         return [
             Column::make("#", "id")->hideIf(true),
-            Column::make("#", "event.id")->hideIf(true),
-            Column::make("Nombre de Evento", "event.name"),
-            Column::make("fecha de Inicio", "event.start_date"),
+            Column::make("Nombre", "user.name"),
+            Column::make("Apellido", "user.surname"),
+            Column::make("Dni", "user.dni"),
+            Column::make("Mail", "user.email"),
             Column::make("Fecha de Inscripcion", "inscription_date"),
-            Column::make("Modalidad", "event.eventModality.description"),
-            Column::make("Link de evento", "event.meeting_link"),
-            Column::make("Ubicacion", "event.venue"),
-            ButtonGroupColumn::make('acciones')
-            ->attributes(function($row) {
-                return [
-                    'class' => 'space-x-2 ',
-                ];
-            })
-            ->buttons([
-                LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
-                    ->title(fn($row) => 'ver Evento')
-                    ->location(fn ($row) => route('evento', ['id' => $row['event.id']]))
-                    ->attributes(function($row) {
-                        return [
-                            'class' =>' border border-1 border-black rounded p-2 text-blue-100 hover:no-underline',
-                        ];
-                    }),
-            
-                LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
-                ->title(fn($row) => 'ver PyR')
-                ->location(fn ($row) => route('evento', ['id' => $row['event.id']]))
-                ->attributes(function($row) {
-                    return [
-                        'class' =>'text-green-500 border border-1 border-black rounded p-2 text-blue-100 hover:no-underline',
-                        'wire:click' => '$emit("showPyRModal")',
-                    ];
-                }),
-            ])->collapseOnMobile()
         ];
     }
 
