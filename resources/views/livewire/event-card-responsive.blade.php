3b79147c523f7a472b9a54c7531377b9a0a56d7f
santiagoavilez <santiagolingreed@gmail.com>
2023-04-04 18:06:41 -0300

    fix infinite scroll home

--- resources/views/livewire/event-card-responsive.blade.php
+++ resources/views/livewire/event-card-responsive.blade.php
@@ -3,18 +3,18 @@
         @php
             $src = $event['image_flyer'];
             if ($event['image_flyer'] == null) {
-                $src = 'images/public/event-card-placeholder.png';
+                $src = 'images/eventFlyers/TUDW_charla2110.jpg';
             }
-
+            
             $endorsementRequest = $event->endorsementRequest;
         @endphp
 
         <img class="rounded-lg" src="{{ asset($src) }}" alt="">
-        
-        
 
-        @if(!is_null($endorsementRequest))
-            @if($endorsementRequest->endorsed)
+
+
+        @if (!is_null($endorsementRequest))
+            @if ($endorsementRequest->endorsed)
                 <div class="absolute top-4 right-4 z-10">
                     @livewire('verified-badge', ['endorsementRequest' => $endorsementRequest, 'academicUnits' => $academicUnits])
                 </div>
@@ -24,10 +24,11 @@
 
     @foreach ($users as $user)
         @if ($user->id === $event->user_id)
-        <div class="flex items-center  text-white-ghost bg-fogra-dark pt-2 montserrat uppercase ">
-            <img class="h-10 w-10 rounded-full object-cover" src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}"/>
-            <h1 class="truncate ml-2">{{$user->name.' '.$user->surname}}</h1>
-        </div>
+            <div class="flex items-center  text-white-ghost bg-fogra-dark pt-2 montserrat uppercase ">
+                <img class="h-10 w-10 rounded-full object-cover" src="{{ $user->profile_photo_url }}"
+                    alt="{{ $user->name }}" />
+                <h1 class="truncate ml-2">{{ $user->name . ' ' . $user->surname }}</h1>
+            </div>
         @endif
-    @endforeach    
+    @endforeach
 </div>
